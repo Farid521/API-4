bf04911a0204cf9a21899cbbc8ff62edf091c27d
Farid <faritfir521@gmail.com>
2023-05-30 09:50:59 +0700

    menambahkan ip

--- logger.js
+++ logger.js
@@ -1,9 +1,10 @@
 const logger = (req, res, next) => {
   const method = req.method;
   const url = req.url;
+  const ip = req.ip;
   const time = new Date().getFullYear();
-  console.log(`[${time}] ${method} ${url}`);
+  console.log(`[${time}] ${method} ${url} ${ip}`);
   next();
 };
 
-module.exports = logger
+module.exports = logger;
